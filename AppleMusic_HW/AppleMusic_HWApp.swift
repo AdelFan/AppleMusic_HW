3d555a2c29407b6ff69e355937040b6fe7207c45
Drdev <fallen.bmxer@gmail.com>
2021-12-07 13:49:18 +0500

    Initial Commit

---
+++ AppleMusic_HW/AppleMusic_HWApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusic_HWApp.swift
+//  AppleMusic_HW
+//
+//  Created by Адель Ахметшин on 07.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusic_HWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
